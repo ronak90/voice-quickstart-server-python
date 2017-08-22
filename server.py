9768203f12d21af9401c097c31a8ca2726e13427
Ronak Kotecha <ronak.k@simformsolutions.com>
2017-08-22 08:26:51 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 import os
-from flask import Flask, request
+from flask import Flask, Response , request
 from twilio.jwt.access_token import AccessToken, VoiceGrant
 from twilio.rest import Client
 import twilio.twiml
+import json
+from json import JSONEncoder
+
 
 ACCOUNT_SID = 'AC***'
 API_KEY = 'SK***'
@@ -10,28 +13,45 @@ API_KEY_SECRET = '***'
 PUSH_CREDENTIAL_SID = 'CR***'
 APP_SID = 'AP***'
 
-IDENTITY = 'voice_test'
-CALLER_ID = 'quick_start'
 
 app = Flask(__name__)
 
 @app.route('/accessToken')
 def token():
+                          
   account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
   api_key = os.environ.get("API_KEY", API_KEY)
   api_key_secret = os.environ.get("API_KEY_SECRET", API_KEY_SECRET)
   push_credential_sid = os.environ.get("PUSH_CREDENTIAL_SID", PUSH_CREDENTIAL_SID)
   app_sid = os.environ.get("APP_SID", APP_SID)
+  
 
   grant = VoiceGrant(
     push_credential_sid=push_credential_sid,
     outgoing_application_sid=app_sid
   )
-
-  token = AccessToken(account_sid, api_key, api_key_secret, IDENTITY)
+  
+  identity = request.form['socialId']
+  
+  
+  token = AccessToken(account_sid, api_key, api_key_secret, identity)
   token.add_grant(grant)
 
-  return str(token)
+  response={'identity':identity,'token':str(token)}
+  return Response(json.dumps(response), mimetype='application/json')
+
+  
+@app.route("/voice",methods=['GET', 'POST'])
+def voice():
+      
+    IDENTITY = request.form['To']
+    CALLER_ID = '+12672143092'
+    
+    resp = twilio.twiml.Response()   
+    dial = resp.dial(callerId=CALLER_ID) 
+    dial.number(IDENTITY)  
+    
+    return Response(str(resp), mimetype='text/xml')
 
 @app.route('/outgoing', methods=['GET', 'POST'])
 def outgoing():
@@ -52,7 +72,7 @@ def placeCall():
   api_key_secret = os.environ.get("API_KEY_SECRET", API_KEY_SECRET)
 
   client = Client(api_key, api_key_secret, account_sid)
-  call = client.calls.create(url=request.url_root + 'incoming', to='client:' + IDENTITY, from_='client:' + CALLER_ID)
+  call = client.calls.create(url=request.url_root + 'incoming',to='client:' + 'receiver', from_='client:' + 'sender')
   return str(call.sid)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -63,4 +83,4 @@ def welcome():
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=port, debug=True)
+app.run(host='0.0.0.0', port=port, debug=True)
